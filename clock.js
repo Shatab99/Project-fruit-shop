816c532e476ea8ddec5cc0097dbfa56a4cc9ea53
shatab <shatabag4749@gmail.com>
2023-07-31 18:43:38 +0600

    Working on admin panel

--- clock.js
+++ clock.js
@@ -1,6 +1,13 @@
 
+function offer(){
+    let offer = document.getElementById("offer").value;
+    offer=document.getElementById("submit").submit();
+
+}
+
 // Set the date we're counting down to
 var countDownDate = new Date("aug 5, 2023 15:37:25").getTime();
+// var countDownDate = new Date(offer()).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
